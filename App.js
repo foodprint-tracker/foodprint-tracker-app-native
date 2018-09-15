a2a5cb02448b00ca80976599c99b7a61dd3d95b0
bakoe <ba.koe96@gmail.com>
2018-09-15 19:58:12 +0200

    Start adding stat elements to homescreen

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, ScrollView, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import { RkButton } from 'react-native-ui-kitten';
+import { RkButton, RkText, RkStyleSheet, RkTheme } from 'react-native-ui-kitten';
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = {
+      statItems: [
+        {
+          name: 'CO2',
+          value: '12.5 kg',
+          background: '#646464'
+        },
+        {
+          name: 'Energy',
+          value: '190 MJ',
+          background: '#D9B601'
+        },
+        {
+          name: 'Water',
+          value: '112 l',
+          background: '#4472EE'
+        },
+      ]
+    };
+  }
+
+  renderStatItem(item) {
+    return (
+      <View style={[styles.statItemContainer, {backgroundColor: item.background}]} key={item.name}>
+        <View>
+          <RkText style={styles.statItemValue}>{item.value}</RkText>
+          <RkText style={styles.statItemName}>{item.name}</RkText>
+        </View>
+      </View>
+    )
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <RkButton onPress={() => this.props.navigation.navigate('Receipt')}>
-          Go to Receipt
-        </RkButton>
-      </View>
+      <ScrollView style={styles.screen}>
+        <View style={styles.statItems}>
+          {this.data.statItems.map(item => this.renderStatItem(item))}
+        </View>
+      </ScrollView>
     );
   }
 }
+
+let styles = RkStyleSheet.create(theme => ({
+  screen: {
+    backgroundColor: theme.colors.screen.scroll,
+    paddingHorizontal: 15,
+  },
+  statItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 15,
+  },
+  statItemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginHorizontal: 5,
+    flex: 1,
+  },
+  statItemValue: {
+    color: 'white',
+    alignSelf: 'flex-end',
+  },
+  statItemName: {
+    color: 'white',
+    alignSelf: 'flex-end',
+  },
+}));
 
 class ReceiptScreen extends React.Component {
   render() {
